aaae7c777ce4175a8f8c5dad436c001688818cf2
AlyasarJabbarli <alyasarmj@code.edu.az>
2022-04-21 10:24:39 +0400

    Ready

---
+++ JSMethods/main.js
@@ -0,0 +1,8 @@
+if (confirm("Press a button!") == true) {  
+    document.body.style.backgroundColor = "black";
+}
+
+let age = prompt("Please enter your age")
+if (age < 18 ) {
+    alert("You are kid")
+}
